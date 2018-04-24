547f0423257c6fdf7957d7faec0358c697892ff1
NanaProud <nana.proud@yandex.ru>
2018-04-24 15:07:06 +0400

    пампам

--- Labirint/Program.cs
+++ Labirint/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-// ААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААА
+// Я поняла, как эта штука работает
 
 namespace Labirint
 {
