6121219d6eb9c12bae3afb6fdfd4b1099695eb44
NanaProud <nana.proud@yandex.ru>
2018-04-24 15:21:35 +0400

    Revert "АААААА"
    This reverts commit 7495217bb3714238fdf152ad62a7ade37a4f7417.

--- Labirint/Program.cs
+++ Labirint/Program.cs
@@ -5,8 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-// ААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААА
-
 namespace Labirint
 {
     class Program
