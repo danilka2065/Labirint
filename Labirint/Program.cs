4f19476c78335d26ea3b59e879438e78755967ec
danilka2065 <dr.semenczoff@yandex.ru>
2018-04-24 14:06:51 +0400

    Add class Point

--- Labirint/Program.cs
+++ Labirint/Program.cs
@@ -8,6 +8,46 @@ namespace Labirint
 {
     class Program
     {
+        class Point
+        {
+            bool north = false;
+            bool west = false;
+            bool south = false;
+            bool east = false;
+            public int x, y;
+
+            public Point(int a, int b)
+            {
+                x = a;
+                y = b;
+            }
+
+            public bool North
+            {
+                get { return north; }
+                set { north = value; }
+            }
+
+            public bool South
+            {
+                get { return south; }
+                set { south = value; }
+            }
+
+            public bool West
+            {
+                get { return west; }
+                set { west = value; }
+            }
+
+            public bool East
+            {
+                get { return east; }
+                set { east = value; }
+            }
+
+        }
+
         static void Main(string[] args)
         {
 
